9b6e38e68cc07be926dd6bd6764548f0cace14e3
Scott Bedard <scottbedard90@gmail.com>
2014-12-23 03:23:37 -0500

    Just incase the JS validation drops the ball...

--- models/Tag.php
+++ models/Tag.php
@@ -1,12 +1,14 @@
 <?php namespace Bedard\BlogTags\Models;
 
 use Model;
+use ValidationException;
 
 /**
  * Tag Model
  */
 class Tag extends Model
 {
+    use \October\Rain\Database\Traits\Validation;
 
     /**
      * @var string The database table used by the model.
@@ -19,10 +21,24 @@ class Tag extends Model
     public $belongsToMany = [
         'posts' => ['RainLab\Blog\Models\Post', 'table' => 'rainlab_blog_posts', 'order' => 'title']
     ];
-    
+
     /**
      * @var array Fillable fields
      */
     public $fillable = ['name'];
+
+    /**
+     * @var array Validation rules
+     */
+    public $rules = [
+        'name' => 'required|regex:/^[a-z0-9-]+$/'
+    ];
+
+    /**
+     * @var array Validation error messages
+     */
+    public $customMessages = [
+        'name.regex' => 'Tags may contain only alpha-numeric characters and hyphens.'
+    ];
 
 }
