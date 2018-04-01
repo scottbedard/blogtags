8e682198f8c87b0b06f75740213b504f8a012444
Grapxnet <macitsari@gmail.com>
2018-04-01 06:25:57 +0300

    lang.php
    Turkish language file

---
+++ lang/tr/lang.php
@@ -0,0 +1,27 @@
+<?php
+return [
+    //
+    // plugin
+    //
+    'plugin' => [
+        'name' => 'Blog Etiket Eklentsi',
+        'description' => 'RainLab.Blog modülü etiket eklentisi.',
+    ],
+
+    //
+    // form
+    //
+    'form' => [
+        'label' => 'Etiketler',
+        'name_invalid' => 'Etiket adları yalnızca alfasayısal karakterler ve kısa çizgiler içerebilir...',
+        'name_required' => 'Etiket alanı gerekli.',
+        'name_unique' => 'Bu etiket daha önce oluşturulmuş',
+    ],
+
+    //
+    // navigation
+    //
+    'navigation' => [
+        'tags' => 'Etiketler',
+    ]
+];
